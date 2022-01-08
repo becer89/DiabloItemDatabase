0d9f4a1ffdffe0d9484f618d0f98e1d1a0ba3a17
becer <piekarzewski@gmail.com>
2022-01-08 02:30:20 +0100

    disable debug mode

--- DiabloItemDatabase/DiabloItemDatabase/settings.py
+++ DiabloItemDatabase/DiabloItemDatabase/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x853#!u0px%6&s603o7kpvg)38uft)b7^8dk)#hum#k8q%v28s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
